4d57379688490fef3156993e3b06438236888f5e
Sciencmine <2631197967@qq.com>
2019-09-05 22:00:30 +0800

    Update bfs.cpp
    Use vector to reduce memory usage
    Use more function to simplify the code

--- exercise/13/bfs.cpp
+++ exercise/13/bfs.cpp
@@ -1,60 +1,60 @@
 #include <cstdio>
-#include <iostream>
-#include <cstring>
-#include <algorithm>
-#include <cmath>
+#include <vector>
 using namespace std;
 
-struct cood{
+struct node{
 	int x, y, step, last;
-}queue[10001];
+};
+
 int n, m, sx, sy, ex, ey, dir[4][2] = {-1, 0, 0, 1, 1, 0, 0, -1};
 char map[101][101];
-int book[101][101] = {0};
+bool book[101][101] = {0};
+vector<node> queue;
+
+node newNode(int x, int y, int step, int last){
+	register node i;
+	i.x = x, i.y = y, i.step = step, i.last = last;
+	return i;
+}
 
 bool check(int x, int y){
-	if(x < 0 || y < 0 || x >= n || y >= m || map[x][y] == 'X' || book[x][y] == 1){
+	if(x < 0 || y < 0 || x >= n || y >= m || map[x][y] == 'X' || book[x][y]){
 		return false;
 	}
 	return true;
 }
 
-void print(int index){
-	if(index == 0){
-		printf("(%d, %d)\n", queue[index].x, queue[index].y);
+void printTrack(int index){
+	if(index != 0){
+		printTrack(queue[index].last);
 	}
-	else{
-		print(queue[index].last);
-		printf("(%d, %d)\n", queue[index].x, queue[index].y);
-	}
+	printf("(%d, %d)\n", queue[index].x, queue[index].y);
+	return;
 }
 
 int bfs(int sx, int sy){
-	int head = 0, tail = 0;
-	cood start;
-	start.x = sx, start.y = sy, start.step = 1, start.last = 0;
-	queue[tail++] = start;
-	book[sx][sy] = 1;
-	while(head < tail){
-		cood temp = queue[head];
-		head++;
+	register unsigned int head = 0;
+	register int nx, ny;
+	node s = newNode(sx, sy, 1, 0);
+	queue.push_back(s);
+	book[sx][sy] = true;
+	while(head < queue.size()){
+		node t = queue[head++];
 		
 		//printf("Going to (%d, %d), %d step\n", temp.x, temp.y, temp.step);
 		
-		if(temp.x == ex && temp.y == ey){
-			print(head - 1);
-			return temp.step;
+		if(t.x == ex && t.y == ey){
+			printTrack(head - 1);
+			return t.step;
 		}
 		
-		int nx, ny;
 		for(int i = 0; i < 4; i++){
-			nx = temp.x + dir[i][0], ny = temp.y + dir[i][1];
+			nx = t.x + dir[i][0], ny = t.y + dir[i][1];
 			
 			if(check(nx, ny)){
-				book[nx][ny] = 1;
-				cood t;
-				t.x = nx, t.y = ny, t.step = temp.step + 1, t.last = head - 1;
-				queue[tail++] = t;
+				book[nx][ny] = true;
+				node n = newNode(nx, ny, t.step + 1, head - 1);
+				queue.push_back(n);
 			}
 		}
 	}
@@ -66,15 +66,13 @@ int main(){
 	scanf("%d %d", &n, &m);
 	
 	for(int i = 0; i < n; i++){
-		cin >> map[i];
+		scanf(" %s", map[i]);
 	}
 	
 	sx = 0, sy = 0;
 	ex = n - 1, ey = m - 1;
 	
-	book[sx][sy] = 1;
-	
-	cout << bfs(sx, sy) << endl;
+	printf("%d\n", bfs(sx, sy));
 	
 	return 0;
 }
