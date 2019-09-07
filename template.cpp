918916fb8aa2660370e61d1434c6e620ace10264
Sciencmine <2631197967@qq.com>
2019-09-07 13:47:12 +0800

    Create template

---
+++ template.cpp
@@ -0,0 +1,38 @@
+/*
+    cpp-study - a repository for storing code written during my cpp learning
+    Copyright (C) 2019 Sciencmine
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+#include <string>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+#define uns unsigned
+
+const char lf = '\n';
+
+const int Inf = 0x3f3f3f3f;
+
+int main() {
+	std::ios::sync_with_stdio(false);
+	//freopen("" ".in", "r", stdin);
+	//freopen("" ".out", "w", stdout);
+
+	return 0;
+}
